eedd9676ec95c946529512b301f5ef73bbb31519
hema-2508 <hema.acharya2508@gmail.com>
2025-08-11 11:34:35 +0530

    Calculator

---
+++ calculator.js
@@ -0,0 +1,40 @@
+
+const display = document.getElementById('display');
+
+function addToDisplay(value) {
+    display.value += value;
+    console.log('Added to display: ' + value);
+    console.log('Current expression: ' + display.value);
+}
+
+function clearDisplay() {
+    display.value = '';
+    console.log('Display cleared');
+}
+
+function deleteLastChar() {
+    const oldValue = display.value;
+    display.value = display.value.toString().slice(0, -1);
+    console.log('Deleted last character from: ' + oldValue);
+    console.log('Current expression: ' + display.value);
+}
+
+function calculateResult() {
+    try {
+        const expression = display.value;
+        const result = eval(expression);
+        
+        console.log('=====================');
+        console.log('CALCULATION PERFORMED:');
+        console.log('Expression: ' + expression);
+        console.log('Result: ' + result);
+        console.log('=====================');
+        
+        display.value = result;
+        
+    } catch (error) {
+        console.error('Calculation Error: ' + error.message);
+        display.value = 'Error';
+    }
+}
+
